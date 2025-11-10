33511413241d83723c85d841728aa94c055554ea
Atharvk08 <atharva.kharapkar@gmail.com>
2025-11-11 00:23:19 +0530

    initial commit

---
+++ src/main/java/com/ak/kafka_sample_ed/KafkaSampleEdApplication.java
@@ -0,0 +1,13 @@
+package com.ak.kafka_sample_ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSampleEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSampleEdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ak/kafka_sample_ed/KafkaSampleEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.kafka_sample_ed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSampleEdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
